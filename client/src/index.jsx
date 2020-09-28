a699be406630b9a213d7e14338e851e2b9616d61
Wassim-Nahdi <wassimnahdi@gmail.com>
2020-09-28 20:10:30 +0100

    basic ground

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react' 
-import ReactDOM from 'react-dom'
-import Home from './components/Home.jsx'
+import ReactDOM from 'react-dom';
+import Chat from './components/Chat.jsx';
+import Home from './components/Home.jsx';
+import { BrowserRouter, Route, Switch,Link } from 'react-router-dom';
+import Tasks from './components/Tasks.jsx';
+import Todo from './components/Todo.jsx';
 
 class App extends React.Component{
     constructor(props){
         super(props)
     }
     render(){
-        return <Home/>
+        return (
+            <div>
+                <Chat/>
+                <Tasks/>
+                <Todo/>
+        </div>
+        )
     }
 }
-export default App
+export default App;
 
 
 ReactDOM.render(<App/> , document.getElementById('app'))
